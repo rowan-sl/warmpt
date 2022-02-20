c6237c4f104a75f6f3ed7fd6c9ef044fcfbe34b5
Rowan S-L <rowan.with.cats@gmail.com>
2022-02-20 15:35:27 -0500

    add logging

--- src/main.rs
+++ src/main.rs
@@ -4,19 +4,24 @@ pub mod types;
 pub mod world;
 
 extern crate image;
+extern crate pretty_env_logger;
 extern crate stati;
 extern crate yaml_rust;
+#[macro_use]
+extern crate log;
 
 use std::fs::OpenOptions;
 use std::io::Read;
-use std::time::Duration;
+use std::path::Path;
+use std::time::{Duration, Instant};
 
 use image::codecs::gif::GifEncoder;
 use image::{codecs::gif::Repeat, Delay, DynamicImage, Frame};
 use yaml_rust::{Yaml, YamlLoader};
+use stati::{BarManager, bars, prelude::*};
 
 use tile::Tile;
-use world::{WorldBuilder, World};
+use world::{World, WorldBuilder};
 
 fn parse_tile(src: &Vec<Yaml>) -> Option<Tile> {
     if src.len() != 3 {
@@ -55,21 +60,22 @@ fn load_world(config: Yaml) -> Option<(World, Config)> {
     let sim_substeps = config["sim_substeps"].as_i64()? as u64;
     let max_display_heat = config["max_display_heat"].as_f64()? as f32;
     let world_size_raw = config["world_size"].as_vec()?;
+    info!("Boaded world properties");
     let world_size = (
         world_size_raw[0].as_i64()? as u64,
         world_size_raw[1].as_i64()? as u64,
     );
     let default_tile = parse_tile(config["default_tile"].as_vec()?)?;
 
-    let mut world_builder = WorldBuilder::with_default_tile(
-        world_size.0 as usize,
-        world_size.1 as usize,
-        default_tile,
-    );
+    let mut world_builder =
+        WorldBuilder::with_default_tile(world_size.0 as usize, world_size.1 as usize, default_tile);
 
     let build_instructions_raw = config["build_instructions"].as_vec()?;
 
+    info!("Building world..");
+
     for raw_inst in build_instructions_raw {
+        trace!("Executing build instruction: {:?}", raw_inst);
         let inst = raw_inst.as_vec()?;
         if inst.len() == 0 {
             panic!()
@@ -108,43 +114,90 @@ fn load_world(config: Yaml) -> Option<(World, Config)> {
         }
     }
 
-    Some((world_builder.build(), Config {
-        out_file: out_file.into(),
-        gif_repeat,
-        sim_steps,
-        sim_substeps,
-        max_display_heat,
-        frame_delay,
-    }))
+    Some((
+        world_builder.build(),
+        Config {
+            out_file: out_file.into(),
+            gif_repeat,
+            sim_steps,
+            sim_substeps,
+            max_display_heat,
+            frame_delay,
+        },
+    ))
 }
 
-fn load_config() -> anyhow::Result<Yaml> {
-    let args = std::env::args().collect::<Vec<String>>();
-    let path = if let Some(v) = args.get(1) {v} else {anyhow::bail!("Missing config argument!");};
-    println!("Reading config from {}", path);
-    let mut conf_file = OpenOptions::new()
-        .read(true)
-        .create(false)
-        .open(path)?;
+fn load_config<P: AsRef<Path>>(path: P) -> anyhow::Result<Yaml> {
+    let mut conf_file = OpenOptions::new().read(true).create(false).open(path)?;
     let mut conf_txt = String::new();
     conf_file.read_to_string(&mut conf_txt)?;
     Ok(YamlLoader::load_from_str(&conf_txt)?.remove(0))
 }
 
+fn get_path() -> anyhow::Result<String> {
+    let args = std::env::args().collect::<Vec<String>>();
+    let path = if let Some(v) = args.get(1) {
+        v
+    } else {
+        anyhow::bail!("Missing config argument!");
+    };
+    Ok(path.clone())
+}
+
 fn main() -> anyhow::Result<()> {
-    let (mut world, config) = if let Some(v) = load_world(load_config()?) {v} else {anyhow::bail!("Could not load world! (invalid config)")};
+    pretty_env_logger::try_init_custom_env("WARMPT_LOG")?;
+    info!("Initializing simulation");
+    let path = get_path()?;
+    debug!("Loading configuration from {path}..");
+    let conf_file = load_config(path)?;
+    debug!("Loaded configuration");
+    debug!("Loading world...");
+    let (mut world, config) = if let Some(v) = load_world(conf_file) {
+        v
+    } else {
+        error!("Invalid Configuration!");
+        anyhow::bail!("Could not load world! (invalid config)")
+    };
+    debug!("Loaded world");
 
-    let render_file = OpenOptions::new().write(true).create(true).open(config.out_file).unwrap();
+    info!("Creating render file {}", config.out_file);
+    let render_file = OpenOptions::new()
+        .write(true)
+        .create(true)
+        .open(config.out_file)?;
     let mut encoder = GifEncoder::new(render_file);
-    encoder.set_repeat(if config.gif_repeat {Repeat::Infinite} else {Repeat::Finite(0)}).unwrap();
+    encoder
+        .set_repeat(if config.gif_repeat {
+            Repeat::Infinite
+        } else {
+            Repeat::Finite(0)
+        })?;
 
-    for _ in 0..config.sim_steps {
+    debug!("Created render file");
+
+    info!("Starting simulation...");
+
+    debug!("Simulation running for {} frames, with {} steps per frame", config.sim_steps, config.sim_substeps);
+
+    let before = Instant::now();
+
+    let mut bman = BarManager::new();
+    for _ in (0..config.sim_steps).display_bar(bman.register(bars::SimpleBar::new(&"Simulating", 0))) {
         for _ in 0..config.sim_substeps {
             world.tick();
         }
+        bman.print();
         let img = DynamicImage::ImageRgb8(world.observe(config.max_display_heat)).into_rgba8();
-        encoder.encode_frame(Frame::from_parts(img, 0, 0, config.frame_delay)).unwrap();
+        encoder
+            .encode_frame(Frame::from_parts(img, 0, 0, config.frame_delay))
+            .unwrap();
     }
+
+    let after = Instant::now();
+
+    debug!("Simulation ran in {:?}", after - before);
+
+    info!("done!");
 
     Ok(())
 }
